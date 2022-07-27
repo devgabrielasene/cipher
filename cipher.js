88794284771a53aefe025d6137c586115135c082
devgabrielasene <gabrielasjjx@gmail.com>
2022-07-27 01:10:45 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -11,9 +11,14 @@ encode: function encode(moves, writing) {
       cripMessage = cripMessage + String.fromCharCode(charCode);
     } 
     
-    else {
-      alert ("Escreva somente com letras maiúsculas e não utilize espaços");
-      break
+    else if (writing.charCodeAt(i) >= 97 && writing.charCodeAt(i) <= 122) {
+      let atCode = ((((writing.charCodeAt(i) - 97) + movesDone) % 26) + 97);
+      cripMessage = cripMessage + String.fromCharCode(atCode);
+    }
+    else if (writing.charCodeAt(i) >= 32 && writing.charCodeAt(i) <= 64) {
+      let movesDone = (moves - (parseInt (moves / 33)) * 33);
+      let caraCode = ((writing.charCodeAt(i) - 32 + movesDone) % 33) + 32;
+      cripMessage += String.fromCharCode(caraCode);
     }
   }
   return cripMessage;
@@ -21,19 +26,27 @@ encode: function encode(moves, writing) {
 
 decode: function decode(moves, writing) {
   if (typeof moves !== "number" || typeof writing !== "string") throw new TypeError();
-
   let descripMessage = "";
+
   for (let i = 0; i < writing.length; i++) {
     let movesDone = (moves - (parseInt (moves / 26)) * 26);
 
      if (writing.charCodeAt(i) >= 65 && writing.charCodeAt(i) <= 90) {
         let charCode = (((((writing.charCodeAt(i) - 65) - movesDone) +  26) % 26) + 65);
         descripMessage = descripMessage + String.fromCharCode(charCode);
-      } else {
-          alert('Escreva somente com letras maiúsculas');
-          break;
+      } 
+      
+      else if (writing.charCodeAt(i) >= 97 && writing.charCodeAt(i) <= 122) {
+        let atCode = (((((writing.charCodeAt(i) - 97) - movesDone) + 26) % 26) + 97);
+        descripMessage = descripMessage + String.fromCharCode(atCode);
       }
-    } 
+      else if (writing.charCodeAt(i) >= 32 && writing.charCodeAt(i) <= 64) {
+        let movesDone = (moves - (parseInt (moves / 33)) * 33);
+        let caraCode = (((((writing.charCodeAt(i) - 32) - movesDone) + 33) % 33) + 32);
+        descripMessage += String.fromCharCode(caraCode);
+      }
+
+      } 
     return descripMessage;
   }
 }
