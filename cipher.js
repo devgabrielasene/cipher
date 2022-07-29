d853291e12d15f709637fd509479cc47221c58ed
devgabrielasene <gabrielasjjx@gmail.com>
2022-07-29 00:44:29 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -7,17 +7,17 @@ encode: function encode(moves, writing) {
     let movesDone = (moves - (parseInt (moves / 26)) * 26);
 
     if (writing.charCodeAt(i) >= 65 && writing.charCodeAt(i) <= 90) {
-      let charCode = ((((writing.charCodeAt(i) - 65) + movesDone) % 26) + 65);
+      const charCode = ((((writing.charCodeAt(i) - 65) + movesDone) % 26) + 65);
       cripMessage = cripMessage + String.fromCharCode(charCode);
     } 
     
     else if (writing.charCodeAt(i) >= 97 && writing.charCodeAt(i) <= 122) {
-      let atCode = ((((writing.charCodeAt(i) - 97) + movesDone) % 26) + 97);
+      const atCode = ((((writing.charCodeAt(i) - 97) + movesDone) % 26) + 97);
       cripMessage = cripMessage + String.fromCharCode(atCode);
     }
     else if (writing.charCodeAt(i) >= 32 && writing.charCodeAt(i) <= 64) {
       let movesDone = (moves - (parseInt (moves / 33)) * 33);
-      let caraCode = ((writing.charCodeAt(i) - 32 + movesDone) % 33) + 32;
+      const caraCode = ((writing.charCodeAt(i) - 32 + movesDone) % 33) + 32;
       cripMessage += String.fromCharCode(caraCode);
     }
   }
@@ -32,17 +32,17 @@ decode: function decode(moves, writing) {
     let movesDone = (moves - (parseInt (moves / 26)) * 26);
 
      if (writing.charCodeAt(i) >= 65 && writing.charCodeAt(i) <= 90) {
-        let charCode = (((((writing.charCodeAt(i) - 65) - movesDone) +  26) % 26) + 65);
+        const charCode = (((((writing.charCodeAt(i) - 65) - movesDone) +  26) % 26) + 65);
         descripMessage = descripMessage + String.fromCharCode(charCode);
       } 
       
       else if (writing.charCodeAt(i) >= 97 && writing.charCodeAt(i) <= 122) {
-        let atCode = (((((writing.charCodeAt(i) - 97) - movesDone) + 26) % 26) + 97);
+        const atCode = (((((writing.charCodeAt(i) - 97) - movesDone) + 26) % 26) + 97);
         descripMessage = descripMessage + String.fromCharCode(atCode);
       }
       else if (writing.charCodeAt(i) >= 32 && writing.charCodeAt(i) <= 64) {
         let movesDone = (moves - (parseInt (moves / 33)) * 33);
-        let caraCode = (((((writing.charCodeAt(i) - 32) - movesDone) + 33) % 33) + 32);
+        const caraCode = (((((writing.charCodeAt(i) - 32) - movesDone) + 33) % 33) + 32);
         descripMessage += String.fromCharCode(caraCode);
       }
 
